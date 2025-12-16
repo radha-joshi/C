94875e6fb711bdba10ca79d294ef22f705b603d3
Radha Joshi <radha.joshi.1907@gmail.com>
2025-12-16 20:12:44 +0530

    new

--- program-1/main.c
+++ program-1/main.c
@@ -1,8 +1,41 @@
-//printf function
-#include <stdio.h>
-#define PI 3.14 //preprocessor directive: macro
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+     int data;
+     struct node *next;
+};
 
-int main(void) {
-    printf("My First C Program! %f", PI);
+struct node *head=NULL;
+struct node *ptr;
+
+void insert_at_beg()
+{
+    ptr = (struct node *)malloc(sizeof (struct node));
+    if (ptr == NULL)
+    {
+        printf("failed to allocate memory");
+    }
+    else
+    { 
+        printf("enter data: \n");
+        scanf("%d", &ptr->data);
+        if(head==NULL)
+        {
+            head=ptr;
+            ptr->next=NULL;
+        }
+        else
+        {
+            ptr->next=head;
+            head=ptr;
+        }
+    }
+}
+
+int main()
+{
+    insert_at_beg();
     return 0;
 }
+
